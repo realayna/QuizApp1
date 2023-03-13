4e2608183310b2a087455b81e8728e963e2705fb
realayna <100434027+realayna@users.noreply.github.com>
2023-03-13 21:58:57 +0100

    final

--- QuizApp/Form1.Designer.cs
+++ QuizApp/Form1.Designer.cs
@@ -32,70 +32,82 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::QuizApp.Properties.Resources.gdansk;
-            this.pictureBox1.Location = new System.Drawing.Point(137, 47);
+            this.pictureBox1.Location = new System.Drawing.Point(179, 28);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(466, 313);
+            this.pictureBox1.Size = new System.Drawing.Size(448, 317);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.checkAnswerEvent);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(137, 417);
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(179, 452);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 64);
+            this.button1.Size = new System.Drawing.Size(128, 75);
             this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(431, 417);
+            this.button2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Location = new System.Drawing.Point(335, 452);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(172, 64);
+            this.button2.Size = new System.Drawing.Size(128, 75);
             this.button2.TabIndex = 2;
+            this.button2.Tag = "2";
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(137, 519);
+            this.button3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(499, 452);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(172, 64);
+            this.button3.Size = new System.Drawing.Size(128, 75);
             this.button3.TabIndex = 3;
+            this.button3.Tag = "3";
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
             // 
-            // button4
+            // label1
             // 
-            this.button4.Location = new System.Drawing.Point(431, 519);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(172, 64);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(203, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Question";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 609);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Quiz App";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -105,7 +117,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- QuizApp/Form1.cs
+++ QuizApp/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.SymbolStore;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,126 @@ namespace QuizApp
 {
     public partial class Form1 : Form
     {
+        //variables
+        int cAnswer;
+        int qNumber = 1;
+        int score;
+        int totalQuestions;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            askQuestion(qNumber);
+            totalQuestions = 8;
         }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+            if (buttonTag == cAnswer)
+            {
+                score++;
+            }
+            if (qNumber == totalQuestions)
+            {
+
+
+                MessageBox.Show(
+                    "Quiz Ended!" + Environment.NewLine +
+                    "You have answered " + score + " questions correctly." + Environment.NewLine +
+
+              
+                    );
+                score = 0;
+                qNumber = 0;
+                askQuestion(qNumber);
+            }
+            qNumber++;
+            askQuestion(qNumber);
+        }
+        private void askQuestion(int qnum)
+        {
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.gdansk;
+                    label1.Text = "Which city is demonstrated on the picture?";
+                    button1.Text = "Gdansk";
+                    button2.Text = "Bydgoszcz";
+                    button3.Text = "Torun";
+                    cAnswer = 1;
+
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.lodz;
+                    label1.Text = "Which city is demonstrated on the picture?";
+                    button1.Text = "Warsawa";
+                    button2.Text = "Szczecin";
+                    button3.Text = "Lodz";
+
+                    cAnswer = 3;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.bydgosh;
+                    label1.Text = "Which city is demonstrated on the picture?";
+                    button1.Text = "Wroclaw";
+                    button2.Text = "Bydgoszcz";
+                    button3.Text = "Torun";
+
+                    cAnswer = 2;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.warsaw;
+                    label1.Text = "Which city is demonstrated on the picture?";
+                    button1.Text = "Szczecin";
+                    button2.Text = "Warszawa";
+                    button3.Text = "Gdynia";
+
+                    cAnswer = 2;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.torun;
+                    label1.Text = "Which city is demonstrated on the picture?";
+                    button1.Text = "Torun";
+                    button2.Text = "Poznan";
+                    button3.Text = "Lublin";
+
+                    cAnswer = 1;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.wroclaw;
+                    label1.Text = "Which city is demonstrated on the picture?";
+                    button1.Text = "Katowice";
+                    button2.Text = "Poznan";
+                    button3.Text = "Wroclaw";
+
+                    cAnswer = 3;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.krakow;
+                    label1.Text = "Which city is demonstrated on the picture?";
+                    button1.Text = "Kielce";
+                    button2.Text = "Wroclaw";
+                    button3.Text = "Krakow";
+
+                    cAnswer = 3;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources.szczecin;
+                    label1.Text = "Which city is demonstrated on the picture?";
+                    button1.Text = "Birmingham";
+                    button2.Text = "Sopot";
+                    button3.Text = "Szczecin";
+
+                    cAnswer = 3;
+                    break;
+            }
+
+        }
+
+       
     }
 }
